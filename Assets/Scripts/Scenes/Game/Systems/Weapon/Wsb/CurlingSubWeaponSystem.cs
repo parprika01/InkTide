a381d0c20d018019a163f454bb6899f38152d8ec
yeerrrr <1574740018@qq.com>
2025-07-02 20:10:31 +0800

    副武器系统更新

---
+++ Assets/Scripts/Scenes/Game/Systems/Weapon/Wsb/CurlingSubWeaponSystem.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class CurlingSubWeaponSystem : MonoBehaviour
+{
+    [Header("事件")]
+    [SerializeField] private BoolEventChannel syncHoldEvent;
+    [Header("参数")]
+    [SerializeField] private float notChargeSpeed = 15f;
+    [SerializeField] private float chargeSpeed = 8f; 
+    [SerializeField] private int notChargeDelay = 4;
+    [SerializeField] private int chargeDelay = 2;
+    [SerializeField] private float maxHoldTime = 0.5f;
+    private Camera cam;
+    private float holdTime;
+    private bool isCharge;
+    private bool isThrow;
+    private bool hasExplosion;
+    private Animator animator;
+    private Rigidbody rb;
+    private ParticleSystem[] explosion;
+
+    private void Awake()
+    {
+        isThrow = true;
+        hasExplosion = false;
+        cam = Camera.main;
+        holdTime = 0;
+        animator = transform.gameObject.GetComponent<Animator>();
+        explosion = GetComponentsInChildren<ParticleSystem>();
+        animator.SetBool("hold", true);
+    }
+    private void FixedUpdate()
+    {
+        if (isThrow)
+        {
+            holdTime += Time.deltaTime;
+            if (holdTime >= maxHoldTime)
+            {
+                isCharge = true;
+            }
+            else
+            {
+                isCharge = false;
+            }
+        }
+    }
+    public void OnEnable()
+    {
+        syncHoldEvent.OnEventRaised += HandleThrow;
+    }
+    public void OnDisable()
+    {
+        syncHoldEvent.OnEventRaised -= HandleThrow;
+    }
+
+    #region EventHandles
+    private void HandleThrow(bool throwInput)
+    {
+        isThrow = throwInput;
+        if (throwInput == false)
+        {
+            Throw();
+        }
+    }
+    #endregion
+
+    async Task OnCollisionEnter(Collision collision)
+    {
+        if (isThrow == false && hasExplosion == false)
+        {
+            hasExplosion = true;
+            int delayTime;
+            if (isCharge)
+            {
+                delayTime = chargeDelay * 1000;
+            }
+            else
+            {
+                delayTime = notChargeDelay * 1000;
+            }
+            await UniTask.Delay(delayTime);
+            Debug.Log("开始爆炸");
+            Bomb();
+            Debug.Log("爆炸后的isThrow: " + isThrow);
+        }
+    }
+    private async Task Throw()
+    {
+        Debug.Log("在手上的时间: " + holdTime);
+        transform.gameObject.AddComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        rb.mass = 0.5f;
+        Vector3 throwDirection = cam.transform.forward; // 获取投掷方向
+        throwDirection.y = 0;
+        transform.SetParent(null);
+        rb.useGravity = true;
+        rb.isKinematic = false;
+        Vector3 forceTest;
+        float ttt;
+        if (isCharge)
+        {
+            forceTest = chargeSpeed * throwDirection;
+            ttt = chargeSpeed;
+        }
+        else
+        {
+            forceTest = notChargeSpeed * throwDirection;
+            ttt = notChargeSpeed;
+        }
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        rb.freezeRotation = true;
+        rb.AddForce(forceTest, ForceMode.Impulse);
+        animator.SetBool("hold", false);
+        Debug.Log("投掷方向: " + throwDirection);
+        Debug.Log("作用在curling上的力: " + forceTest);
+        Debug.Log("施加的速度: " + ttt);
+    }
+    private async void Bomb()
+    {
+        animator.SetTrigger("bomb");
+        foreach (var par in explosion)
+        {
+            par.Play();
+            var em = par.emission;
+            em.enabled = true;
+        }
+        await UniTask.Delay(800);
+        Destroy(transform.gameObject);
+    }
+}
--- Assets/Scripts/Scenes/Game/Systems/Weapon/Wsb/SuctionSubWeaponSystem.cs
+++ Assets/Scripts/Scenes/Game/Systems/Weapon/Wsb/SuctionSubWeaponSystem.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
 using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-public class BurstSubWeaponSystem : MonoBehaviour
+public class SuctionSubWeaponSystem : MonoBehaviour
 {
     [Header("参数")]
-    [SerializeField] private float throwSpeed = 15f;
-    [SerializeField] private float spinSpeed = 0.2f;
+    [SerializeField] private float throwSpeed = 10f;
     [Header("事件")]
     [SerializeField] private BoolEventChannel syncHoldEvent;
+    [Header("基本设置")]
+    [SerializeField] public Transform suctionPoint;  // 吸盘底部位置
+    // [SerializeField] public LayerMask suctionLayers; // 可吸附的层
 
+    private bool isAttached = false;
     private Rigidbody rb;
     private bool isThrow;
     private bool hasExplosion;
     private ParticleSystem[] explosion;
     private Camera cam;
+    private Animator animator;
 
     private void Awake()
     {
         cam = Camera.main;
         explosion = GetComponentsInChildren<ParticleSystem>();
         hasExplosion = false;
+        animator = GetComponent<Animator>();
+        animator.SetBool("hold", true);
     }
 
     public void OnEnable()
@@ -39,42 +45,64 @@ public class BurstSubWeaponSystem : MonoBehaviour
     private void HandleThrow(bool throwInput)
     {
         isThrow = throwInput;
+        animator.SetBool("hold", throwInput);
         Debug.Log("throw: " + isThrow);
         if (throwInput == false)
         {
-            Debug.Log("前");
             Throw();
-            Debug.Log("后");
         }
     }
     #endregion
 
-    void OnCollisionEnter(Collision collision)
+    async Task OnCollisionEnter(Collision collision)
     {
+        if (isAttached) return;
+        ContactPoint contact = collision.contacts[0];
+        Attach(contact.point, contact.normal); // 固定炸弹
+
+        // // 检查是否是可吸附表面
+        // if (((1 << collision.gameObject.layer) & suctionLayers) != 0)
+        // {
+        //     // 获取最近的接触点
+        //     ContactPoint contact = collision.contacts[0];
+
+        //     // 固定炸弹
+        //     Attach(contact.point, contact.normal);
+        // }
         if (isThrow == false && hasExplosion == false)
         {
             hasExplosion = true;
+            await UniTask.Delay(2000);
             Debug.Log("开始爆炸");
             Bomb();
             Debug.Log("爆炸后的isThrow: " + isThrow);
         }
     }
 
+    void Attach(Vector3 point, Vector3 normal)
+    {
+        animator.SetBool("stick", true);
+        isAttached = true;
+        rb.isKinematic = true;
+        // 使炸弹的向上方向与表面法线对齐
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.Cross(transform.right, normal), normal);
+        // 调整位置使吸盘正好接触表面
+        Vector3 positionOffset = point - suctionPoint.position;
+        transform.position += positionOffset;
+        transform.rotation = targetRotation;
+    }
     private void Throw()
     {
         transform.gameObject.AddComponent<Rigidbody>();
         rb = GetComponent<Rigidbody>();
-        rb.mass = 1f;
+        rb.mass = 0.5f;
         Vector3 throwDirection = cam.transform.forward; // 获取投掷方向
         Debug.Log("投掷方向: " + throwDirection);
         transform.SetParent(null);
-        Debug.Log("脱离了上一级物体");
         rb.useGravity = true;
         rb.isKinematic = false;
         rb.AddForce(throwDirection * throwSpeed, ForceMode.Impulse);
-        rb.AddTorque(throwDirection * spinSpeed, ForceMode.Impulse); // 旋转效果
     }
-
     private async void Bomb()
     {
         foreach (var par in explosion)
@@ -82,9 +110,8 @@ public class BurstSubWeaponSystem : MonoBehaviour
             par.Play();
             var em = par.emission;
             em.enabled = true;
-            // par.Play();
         }
-        await UniTask.Delay(800);
+        await UniTask.Delay(500);
         Destroy(transform.gameObject);
     }
 }
